487130d0c437a56903fd247039a34ddca753f1b0
SkyFather <cyh278889789@gmail.com>
2016-12-23 22:22:35 +0800

    two_fighters_one_winner

---
+++ Two fighters, one winner/two_figthers_one_winner.py
@@ -0,0 +1,11 @@
+class Fighter(object):
+
+    def __init__(self, name, health, damage_per_attack):
+        self.name = name
+        self.health = health
+        self.damage_per_attack = damage_per_attack
+
+
+def declare_winner(fighter1, fighter2, first_attacker):
+    # Code your solution here
+    pass
