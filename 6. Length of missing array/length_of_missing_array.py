916a0e724c15c586da2a560ff9369144563f3506
SkyFather <cyh278889789@gmail.com>
2016-12-24 22:26:08 +0800

    update length_of_missing_array

--- 6. Length of missing array/length_of_missing_array.py
+++ 6. Length of missing array/length_of_missing_array.py
@@ -1,9 +1,16 @@
 def get_length_of_missing_array(array_of_arrays):
-    result = []
-    for itr in array_of_arrays:
-        result.append(len(itr))
-    result.sort()
-    return sorted(set(range(result[0], result[-1] + 1)).difference(result))[0]
+
+    if not array_of_arrays:
+        return 0
+    else:
+        result = []
+        for itr in array_of_arrays:
+            if not itr:
+                return 0
+            else:
+                result.append(len(itr))
+        result.sort()
+        return sorted(set(range(result[0], result[-1] + 1)).difference(result))[0]
 
 print(get_length_of_missing_array(
     [[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]]))
