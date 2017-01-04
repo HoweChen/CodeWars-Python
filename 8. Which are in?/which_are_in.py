6e4a1bc3bb4642f133a5b45c7045dc6798d3c47b
SkyFather <cyh278889789@gmail.com>
2017-01-04 18:40:18 +0800

    which are in

--- 8. Which are in?/which_are_in.py
+++ 8. Which are in?/which_are_in.py
@@ -1,3 +1,14 @@
 def in_array(array1, array2):
     # your code
-    return []
+    result = []
+    for item_1 in array1:
+        for item_2 in array2:
+            if item_1 in item_2 and item_1 not in result:
+                result.append(item_1)
+    result = sorted(result)
+    return result
+
+a1 = ["live", "arp", "strong"]
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+r = ['arp', 'live', 'strong']
+print(in_array(a1, a2))
