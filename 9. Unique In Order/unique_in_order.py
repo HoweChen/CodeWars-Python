1cc367a4199da61514455dfebdec55ab3e634e91
SkyFather <cyh278889789@gmail.com>
2017-01-04 22:36:33 +0800

    update

--- 9. Unique In Order/unique_in_order.py
+++ 9. Unique In Order/unique_in_order.py
@@ -21,6 +21,7 @@ def unique_in_order(iterable):
     #         prev = char
     # return result
 
+
 print(unique_in_order('AAAABBBCCDAABBB'))
 print(unique_in_order('ABBCcAD'))
 print(unique_in_order([1, 2, 2, 3, 3]))
