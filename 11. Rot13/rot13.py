45368a902be794d21ed0e375415cd347a6693c6a
SkyFather <cyh278889789@gmail.com>
2017-01-08 16:12:33 +0800

    rot13.py

--- 11. Rot13/rot13.py
+++ 11. Rot13/rot13.py
@@ -1,5 +1,35 @@
 import string
 from codecs import encode as _dont_use_this_
 
+
 def rot13(message):
-    pass
+    result = ""
+    if message:
+        for char in message:
+            if 65 <= ord(char) <= 90 or 97 <= ord(char) <= 122:
+                if 65 <= ord(char) <= 90:
+                    # capitlized case
+                    if ord(char) + 13 > 90:
+                        char = chr(ord(char) + 13 - 90 + 65 - 1)
+                    else:
+                        char = chr(ord(char) + 13)
+                else:
+                    # lowercase
+                    if ord(char) + 13 > 122:
+                        char = chr(ord(char) + 13 - 122 + 97 - 1)
+                    else:
+                        char = chr(ord(char) + 13)
+            else:
+                pass
+            result += char
+    return result
+
+    # better answer:
+
+    # lower = maketrans(lowercase, lowercase[13:] + lowercase[:13])
+    # upper = maketrans(uppercase, uppercase[13:] + uppercase[:13])
+    # return message.translate(lower).translate(upper)
+
+print(rot13("test"))
+print(rot13("Test"))
+print(rot13(""))
